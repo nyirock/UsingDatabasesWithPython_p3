c15ae279f1b609aac5aa54e871f002b84dc1674b
nyirock <nyirock@gmail.com>
2017-07-17 11:06:28 -0600

    initial commit

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "4Se8J018D88z8jWQTOkRKQ5jY",
+            "consumer_secret": "e2w29KZIxRpFpKcqLw5A6GRLTJeaBU0ZyLl9yC2QXYoHUDOQxw",
+            "token_key": "2789693187-XKWOu889GcaeU1BOERBvrRLvfbgvUVkV68da9MW",
+            "token_secret": "FPTfi0UBCQPk2G5CPg3JhiH8VKOITTQECTCMprjs7i7x2"}
---
+++ w5/geodata/geodata/where.js
@@ -0,0 +1,6 @@
+myData = [
+[51.0799762,-114.1256005, 'Biology, Calgary, AB T2N 4V8, Canada'],
+[42.340075,-71.0895367, 'Northeastern, Boston, MA 02115, USA'],
+[38.2113643,-85.7470011, 'Bradley Ave, Louisville, KY, USA'],
+[32.778949,35.019648, 'Technion/ Sports Building, Haifa']
+];
